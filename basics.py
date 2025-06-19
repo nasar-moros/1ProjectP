5f81f4729f11bee220e66f31f7b34d1dbdd16278
Nasar <nazar.moroz.666@gmail.com>
2025-06-20 00:37:22 +0300

    Initial commit:basics.py

---
+++ basics.py
@@ -0,0 +1,23 @@
+print("и так это должно вывестись на экран текстом") # игнор?
+user_name = "Nasar"
+print (user_name)
+user_name = "alex"
+print (user_name)
+print ("now the user is:")
+print(user_name)
+favorite_language = "python"
+print (favorite_language)
+release_year = "1991"
+print(release_year)
+print(favorite_language, release_year)
+release_year = "2024"
+print(release_year)
+product_name = "iphone 15"
+pruduct_count = 2
+pruduct_price = 999.99
+is_in_stock = True
+print(product_name,pruduct_count,pruduct_price,is_in_stock)
+print( type(product_name) )
+print( type(pruduct_count) )
+print( type(pruduct_price) )
+print( type(is_in_stock) )
